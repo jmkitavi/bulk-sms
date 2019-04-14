ba3b24957338bab2466bf8d19ac4aa5dbc243167
sirjmkitavi <kitavimjoseph@gmail.com>
2019-04-14 20:03:59 +0300

    sendMessage function

--- app/Message.js
+++ app/Message.js
@@ -8,8 +8,41 @@ import {
   ToastAndroid,
   TouchableOpacity,
 } from 'react-native'
+import axios from 'axios'
+
+
+const config = {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Accept': 'application/json; charset=utf-8',
+    'apiKey': '7053f05983fef37d2c0fa62a5bd4205586404084b55694d52a4004f750600b1e',
+  }
+}
 
 export default class App extends Component {
+  sendMessage = () => {
+    let numbers = ['254705542919', '254705542919']
+    // const numbers = this.state.numbers.map((num) => num)
+
+    const params = {
+      username: 'katana',
+      to: numbers,
+      message: 'This is a test message.',
+    }
+
+    const postParams = Object.keys(params).map((key) => {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    }).join('&')
+
+    axios.post(
+      'https://api.africastalking.com/version1/messaging',
+      postParams,
+      config,
+    )
+      .then((response) => console.log('axios response', response))
+      .catch((error) => console.log('axios error', error))
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -36,6 +69,7 @@ export default class App extends Component {
             width: '50%'
           }}
           onPress={() => {
+            this.sendMessage()
             ToastAndroid.show('Message sent Successfully', ToastAndroid.SHORT)
           }}
         >
